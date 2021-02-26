491bc31bfd906ad774afa5bef057ee5b03f8871d
celia22 <celialopez22@gmail.com>
2021-02-26 08:59:00 +0100

    iteration 4 change to correct method

--- src/movies.js
+++ src/movies.js
@@ -16,6 +16,11 @@ let howManyMovies = (arr) => {
     let drama = arr.filter(x => x.genre.includes("Drama"));
     let Steven = drama.filter(x => x.director === "Steven Spielberg");
     let sum = Steven.length;
+/*
+     return movies.filter((movies)=> {
+      return movies.director === "steven " && movie.genre.includes("Drama")
+      return SpilbergDrama */ //MUCHO MAS SIMPLE QUE LO QUE HE HECHO
+    
     return sum
 };
 
@@ -31,43 +36,42 @@ const ratesAverage = (arr) => {
           return 0;
         }
       
-        const rates = arr.map(function (x) {
-          return x.rate;
-        });
-      
-        console.log(rates);
-      
-        let totalRate = rates.reduce(function (acc, el) {
-          if (el) {
-            return acc + el;
+      const rates = arr.map(x => x.rate)     
+     
+      let totalRate = rates.reduce(function (acc, num) {
+          if (num) { // esto es para que cuente cualquier elemento que se le pase
+            //aunque no pueda sumarse al total, pero que cuente para el average
+            // si es un numero(num), añade num y acumulador.
+            return acc + num;
           } else {
-            return acc;
+            return acc; // si no es un numero, añades acumulador
           }
         }, 0);
       
         return Math.round((totalRate / arr.length) * 100) / 100;
 }
       
-      console.log(ratesAverage(movies));
+      //console.log(ratesAverage(movies));
 
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies. 
 
-const dramaMoviesRate = (arr) => {
-    let drama = arr.filter(x => x.genre.includes("Drama"));
-    let rateTotal = drama.map(x => x.rate)
-    let rateav = 0
-    let ratesum = 0;
+const dramaMoviesRate = arr => {
+  let dramaM= arr.filter(x => x.genre.includes("Drama"));
 
-  for(let i = 0; i < rateTotal.length; i++){  
-      ratesum += rateTotal[i]
-      rateav = ratesum / rateTotal.length
-      
-  }; 
-    rateav = Math.round(rateav * 100) / 100
-    return rateav
-};
+      if (dramaM.length === 0) {
+         return 0;
+      }
+ 
+     const dramaMoviesRate = dramaM.map (x => x.rate)   
+
+     let rateDrama = dramaMoviesRate.reduce(function (acc, num) {
+        return acc + num;
+      }, 0);
+
+  return Math.round((rateDrama / dramaM.length) * 100) / 100;
+}
 
 //console.log(dramaMoviesRate(movies));
 
@@ -108,15 +112,6 @@ const orderAlphabetically = (arr) => {
   };       
 };
 //// console.log(orderAlphabetically(movies));
-
-
-
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
-
-
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
 
 
 
